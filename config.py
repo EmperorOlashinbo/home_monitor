a410d08a3fe1c7e0d7c4594fd9f9e90207acf667
EmperorOlashinbo <134160568+EmperorOlashinbo@users.noreply.github.com>
2025-08-11 18:08:55 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Wi-Fi credentials
+SSID = "Tenda_0"
+PASSWORD = "*****"
+
+# ThingSpeak
+THINGSPEAK_API_KEY = "HCN2Y67D5UXWB6KD"
+THINGSPEAK_URL = "http://api.thingspeak.com/update"
+
+# Discord Webhook
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1388876500471779338/g5EbIxK4DMS51WNuOj9oyLCkhpnhy1BPes6Cls923oky7GYrKklIT7YgUh0EOyjNZRdr"
+
+# Thresholds
+TEMP_THRESHOLD = 30
+HUMIDITY_THRESHOLD = 80
+LIGHT_THRESHOLD = 20  # low light
